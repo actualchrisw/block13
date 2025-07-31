bc8324e7ad53f64abb405155798ecb085aa585d8
Chris Warman <actualchrisw@gmail.com>
2025-07-30 20:16:40 -0600

    Add files via upload
    Added prompt for user input

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// Initiate flavor variable
+// Prompt user for input
+let flavor = prompt("Please select your flavors");
 
-const flavor = ["vanilla", "vanilla", "vanilla", "coffee", "strawberry", "strawberry"];
-
-//  Expected output: Vanilla: 3, Coffee: 1, Strawberry: 2
+// Initiate flavorCounter variable
 
 const flavorCounter = {};
 
+// For loop
+
+
 for (let i = 0; i < flavor.length; i++) {
         const flavors = flavor[i];
+        
 //  Add one to the count
+
     if (flavors in flavorCounter) {
         flavorCounter[flavor] += 1
     } else {
